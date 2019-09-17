9856572c5ff76d89306e6bec38dcce10fec1783e
Kathleen Kamali <kathleenkamali@gmail.com>
2019-09-17 18:56:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,7 +11,7 @@ def play(songs)
   input = gets.strip
   if songs.include?(input)
     puts "Playing #{input}"
-  elsif input.to_i.to_s == input.strip
+  elsif input.to_i.to_s == input.strip && input.to_i - 1 <= songs.length
     puts "Playing #{songs[input.to_i - 1]}"
   else
     puts "Invalid input, please try again"
